38f0a77475e218c3dec1fc753884d3227b14daf2
Hee99 <119tpgml@gmail.com>
2025-02-03 14:57:21 +0900

    프로젝트생성성

---
+++ 16b-form-actions/eslint.config.js
@@ -0,0 +1,32 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactRefresh.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      // react-hooks는 아직 flat config 지원하지 않음
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+
+      // react-hooks 규칙 적용
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+];
