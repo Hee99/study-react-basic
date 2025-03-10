f845efab7186d152b6d384e617f232ab9665fd8a
Hee99 <119tpgml@gmail.com>
2025-03-10 15:10:51 +0900

    useOptimistic을 활용한 낙관적 업데이트

--- 16c-adv-practice/src/components/Opinion.jsx
+++ 16c-adv-practice/src/components/Opinion.jsx
@@ -1,16 +1,24 @@
-import { use, useActionState } from 'react';
+import { use, useActionState, useOptimistic } from 'react';
 import { OpinionsContext } from '../store/opinions-context';
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, mode) => (mode === 'up' ? prevVotes + 1 : prevVotes - 1),
+  );
+
   async function upvoteAction() {
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
   }
 
   async function downvoteAction() {
+    setVotesOptimistically('down');
     await downvoteOpinion(id);
   }
+
   const [upvoteState, upvoteFormAction, upvotePending] =
     useActionState(upvoteAction);
   const [downvoteState, downvoteFormAction, downvotePending] =
@@ -45,7 +53,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button
           formAction={downvoteFormAction}
